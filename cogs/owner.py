a2fd2941f785c27f65f70914cad71c3d284f6a73
Joonas <joonas.rautiola@hotmail.com>
2018-10-20 16:08:05 +0300

    owner checks and logout command

--- cogs/owner.py
+++ cogs/owner.py
@@ -8,11 +8,19 @@ class Owner:
         self.client = client
 
     @commands.command(hidden=True)
+    @commands.is_owner()
     async def say(self, ctx, *args):
         channel_id = int(args[0])
         string = " ".join(args[1:])
         channel = self.client.get_channel(channel_id)
         await channel.send(string)
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def logout(self, ctx):
+        print('logout')
+        await ctx.send("Logging out... :wave:")
+        await self.client.logout()
 
 
 def setup(client):
