e5d7fda5b643d645b09deba105fbc21dffafed37
Joonas <joonas.rautiola@hotmail.com>
2018-12-09 19:51:54 +0200

    Updated website

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ from discord.ext import commands
 import json
 from utils import logger as misolog
 
-client = commands.Bot(command_prefix=">")
-extensions = ["cogs.commands", "cogs.owner", "cogs.lastfm", "cogs.apis", "cogs.voice", "cogs.events",
-              "cogs.tictactoe", "cogs.fishy", "cogs.mod", "cogs.user"]
-
 logger = misolog.create_logger(__name__)
 
 with open('dont commit\\keys.txt', 'r') as filehandle:
     TOKEN = json.load(filehandle)["TOKEN"]
 
+client = commands.Bot(command_prefix=">")
+extensions = ["cogs.commands", "cogs.owner", "cogs.lastfm", "cogs.apis", "cogs.voice", "cogs.events",
+              "cogs.tictactoe", "cogs.fishy", "cogs.mod", "cogs.user"]
+
 if __name__ == "__main__":
     for extension in extensions:
         try:
             client.load_extension(extension)
-            logger.info(f"{extension} loaded succesfully")
+            logger.info(f"{extension} loaded successfully")
         except Exception as error:
             logger.error(f"{extension} loading failed [{error}]")
 
